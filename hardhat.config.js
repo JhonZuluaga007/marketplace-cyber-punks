b65c3af56d766a31cd4bea7ddfbf06dde6329e5e
jhonzusa <jzuluaga@pastpost.com>
2022-09-07 19:22:23 -0500

    the project configuration is added with the respective test blockchain

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config()
+
+const projectId = process.env.INFURA_PROJECT_ID
+const privateKey = process.env.DEPLOYER_SIGNER_PRIVATE_KEY
+const apiKeyEtherScan = process.env.API_KEY_ETHER_SCAN
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    rinkeby:{
+      url: `https://rinkeby.infura.io/v3/${projectId}`,
+      accounts:[privateKey]
+    }
+  },
+  etherscan: {
+    apiKey: apiKeyEtherScan
+  }
 };
